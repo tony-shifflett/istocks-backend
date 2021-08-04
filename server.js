7621bbe1f0b9b52d03029e564a15e92b7b0343f6
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-08-04 16:16:23 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
-
+const PORT = process.env.PORT;
 
 const express = require('express');
+const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
-const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
 
 app.get('/', (res, req)=>{
     console.log("Default route")
