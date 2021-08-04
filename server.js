b39c908539349359a190b58bbb7cbedeefc1a6d0
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-08-04 16:48:18 -0500

    stock route stub

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT;
 const express = require('express');
 const morgan = require("morgan");
 const cors = require("cors");
+const axios = require("axios")
 
 const app = express();
 
@@ -15,6 +16,12 @@ app.use(morgan("tiny"));
 app.get('/', (req, res)=>{
     console.log('get--res', res)
     res.send('Default Route')
+})
+
+app.get('/:stock', (req, res)=>{
+    const stock = req.params.stock;
+    console.log('get--res', res);
+    res.send(stock);
 })
 
 app.listen(PORT, ()=>{
